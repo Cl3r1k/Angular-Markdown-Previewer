d720d84ae9305e5d393726306ac7bd04f80bcea0
Cl3r1k <Cl3r1k2@gmail.com>
2017-12-14 19:08:38 +0600

    ✔ Completed UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit, DoCheck {
 
     initApp() {
         // tslint:disable-next-line:max-line-length
-        this.rawText = `Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n\`monospace\`, ~~strikethrough~~ .\n\nShopping list:\n\n* apples\n* oranges\n* pears\n\nNumbered list:\n\n1. apples\n2. oranges\n3. pears\n\nThe rain---not the reign---in\nSpain.`;
+        this.rawText = `Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n\`monospace\`, ~~strikethrough~~ .\n\nShopping list:\n\n* apples\n* oranges\n* pears\n\nNumbered list:\n\n1. apples\n2. oranges\n3. pears\n\nAnd here's some code! :+1:\n\n\`\`\`javascript\n$(function(){\n  $('div').html('I am a div.');\n});\n\`\`\`\n\n\nThe rain---not the reign---in\nSpain.`;
 
         this.updateHtml();
     }
